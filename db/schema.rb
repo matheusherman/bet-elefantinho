b91924fcff22494ea412abc40c761398ac85e723
BandeiraPablo <pablo.bandeira@arcasoltec.com.br>
2026-02-11 14:20:30 -0300

    update: update schema

---
+++ db/schema.rb
@@ -0,0 +1,73 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[8.1].define(version: 2026_02_11_133452) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_catalog.plpgsql"
+
+  create_table "jackpot_entries", force: :cascade do |t|
+    t.decimal "amount", null: false
+    t.datetime "created_at", null: false
+    t.bigint "jackpot_id", null: false
+    t.integer "number", null: false, array: true
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["jackpot_id"], name: "index_jackpot_entries_on_jackpot_id"
+    t.index ["user_id"], name: "index_jackpot_entries_on_user_id"
+  end
+
+  create_table "jackpots", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "draw_at", null: false
+    t.string "status", null: false
+    t.decimal "total_amount", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "transactions", force: :cascade do |t|
+    t.decimal "amount", precision: 15, scale: 2, null: false
+    t.datetime "created_at", null: false
+    t.string "transaction_type", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "wallet_id", null: false
+    t.index ["wallet_id"], name: "index_transactions_on_wallet_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "cpf"
+    t.datetime "created_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "name"
+    t.datetime "remember_created_at"
+    t.datetime "reset_password_sent_at"
+    t.string "reset_password_token"
+    t.integer "role", default: 0, null: false
+    t.datetime "updated_at", null: false
+    t.index ["cpf"], name: "index_users_on_cpf", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "wallets", force: :cascade do |t|
+    t.decimal "balance", precision: 15, scale: 2, default: "0.0", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_wallets_on_user_id"
+  end
+
+  add_foreign_key "jackpot_entries", "jackpots"
+  add_foreign_key "jackpot_entries", "users"
+  add_foreign_key "transactions", "wallets"
+  add_foreign_key "wallets", "users"
+end
