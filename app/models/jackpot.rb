d1b1ad3d8f38437bbf91c803ebc9abb5256252cd
BandeiraPablo <pablo.bandeira@arcasoltec.com.br>
2026-02-11 08:59:23 -0300

    feature: insert Model jackpot

---
+++ app/models/jackpot.rb
@@ -0,0 +1,25 @@
+class Jackpot < ApplicationRecord
+  enum status {
+    waiting: 'waiting',
+    runing: 'runing',
+    finished: 'finished'
+    cancelled: 'cancelled'
+  }
+
+  has_many :bets, dependent: :destroy
+  has_many :users, through: :bets
+
+  validates :status, presence: true
+  validates :total_amount, presence: true
+  validates :draw_at, presence: true
+  validate :draw_at_in_future
+
+  private
+
+  def draw_at_in_future
+    if draw_at.present? && draw_at <= Time.current
+      errors.add(:draw_at, "must be in the future")
+    end
+  end
+
+end
