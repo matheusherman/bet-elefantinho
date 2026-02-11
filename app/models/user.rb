f76570087ef7ed34f6378ae8c917d0e220593a93
kendykvwj <kendykvwj@hotmail.com>
2026-02-11 10:31:21 -0300

    correcao role_enum e user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,21 @@
 class User < ApplicationRecord
-
-  # :confirmable, :lockable, :timeoutable, :trackable, :omniauthable
+  # Devise modules
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  enum role: { user: 0, admin: 1 } # define perfis de usuário
+  # Enum de perfis
+  enum :role, { user: 0, admin: 1 }
 
-
+  # Validações
   validates :name, presence: true, length: { maximum: 100 }
-  validates :cpf, presence: true, uniqueness: true, length: { is: 11 } 
+  validates :cpf, presence: true, uniqueness: true, length: { is: 11 }
   validates :email, presence: true,
                     uniqueness: { case_sensitive: false },
                     format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email" }
 
-
   validates :password, presence: true, length: { minimum: 6 }, if: :password_required?
+
+  # Callbacks
   before_save :downcase_email
 
   private
@@ -24,8 +25,6 @@ class User < ApplicationRecord
   end
 
   def password_required?
-    # Valida senha apenas se for criar novo usuário ou alterar senha
     new_record? || password.present?
   end
 end
-
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  get "up" => "rails/health#show", as: :rails_health_check
+  devise_scope :user do
+    root to: "devise/sessions#new"
+  end
 end
+
