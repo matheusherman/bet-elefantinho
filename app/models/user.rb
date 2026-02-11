664c8dd8020da9c98bede789539fbadb2175d078
kendykvwj <kendykvwj@hotmail.com>
2026-02-11 09:38:24 -0300

    correcao routes e user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,31 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
+  # :confirmable, :lockable, :timeoutable, :trackable, :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :cpf, presence: true
-  validates :email, presence: true 
-                    format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email" },
-                    uniqueness: { case_sensitive: false }
-  validates :has_secure_password, presence: true, uniquess: true
+  enum role: { user: 0, admin: 1 } # define perfis de usuário
 
+
+  validates :name, presence: true, length: { maximum: 100 }
+  validates :cpf, presence: true, uniqueness: true, length: { is: 11 } 
+  validates :email, presence: true,
+                    uniqueness: { case_sensitive: false },
+                    format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email" }
+
+
+  validates :password, presence: true, length: { minimum: 6 }, if: :password_required?
+  before_save :downcase_email
+
+  private
+
+  def downcase_email
+    self.email = email.downcase
+  end
+
+  def password_required?
+    # Valida senha apenas se for criar novo usuário ou alterar senha
+    new_record? || password.present?
+  end
 end
+
